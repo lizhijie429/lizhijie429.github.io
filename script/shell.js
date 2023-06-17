ea0ac3599e07062fbdb2d68224166ac7fc5a15a6
lizhijie429 <632163606@qq.com>
2023-06-18 00:01:57 +0800

    继续优化自动化脚本

--- script/shell.js
+++ script/shell.js
@@ -5,6 +5,7 @@ const shell = require('shelljs')
 
 const argv = minimist(process.argv)
 const commitInfo = 'Auto-commit'
+const originInfo = 'origin'
 
 const envList = ['pnpm', 'git']
 //检查控制台是否以运行对应的命令
@@ -15,19 +16,30 @@ envList.forEach((item) => {
   }
 })
 
-const command = [
-  'pnpm docs:build',
-  'git add .',
-  `git commit -am ${argv.commit ? argv.commit : commitInfo}`,
-  'git push origin main',
-  'git subtree push --prefix docs/public origin docs'
-]
+const command = {
+  build: `pnpm docs:build`,
+  add: `git add .`,
+  commit: `git commit -am ${argv.commit ? argv.commit : commitInfo}`,
+  pushMain: `git push ${argv.origin ? argv.origin : originInfo} main`,
+  pushDocs: `git subtree push --prefix docs/public ${
+    argv.origin ? argv.origin : originInfo
+  } docs`
+}
 // 执行脚本
-for (let index = 0; index < command.length; index++) {
-  const element = command[index]
-  if (shell.exec(element).code !== 0) {
+for (let index = 0; index < Object.keys(command).length; index++) {
+  const key = Object.keys(command)[index]
+  const value = command[key]
+  // 如果传入参数 --docs 则不执行打包及发布过程
+  if (
+    Object.keys(argv).includes('docs') &&
+    argv['docs'] &&
+    (key === 'pushDocs' || key === 'build')
+  ) {
+    continue
+  }
+  if (shell.exec(value).code !== 0) {
     //在控制台输出内容
-    shell.echo(`Error: ${element} failed`)
+    shell.echo(`Error: ${value} failed`)
     shell.exit(1)
   }
 }
