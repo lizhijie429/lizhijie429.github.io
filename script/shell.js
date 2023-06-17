3eafbd317f7929215181688d5e1d632f5e102126
lizhijie429 <632163606@qq.com>
2023-06-17 23:18:47 +0800

    Auto-commit

--- script/shell.js
+++ script/shell.js
@@ -1,41 +1,70 @@
 import { createRequire } from 'module'
 const require = createRequire(import.meta.url)
-let shell = require('shelljs')
-let name = process.argv[2] || 'Auto-commit'
+const minimist = require('minimist')
+const shell = require('shelljs')
 
-//检查控制台是否以运行`git `开头的命令
-if (!shell.which('pnpm')) {
-  //在控制台输出内容
-  shell.echo('Sorry, this script requires pnpm')
-  shell.exit(1)
-}
+const argv = minimist(process.argv)
+const commitInfo = 'Auto-commit'
 
-if (shell.exec('pnpm docs:build').code !== 0) {
-  shell.echo('Error: pnpm docs:build failed')
-  shell.exit(1)
-}
+const envList = ['pnpm', 'git']
+//检查控制台是否以运行对应的命令
+envList.forEach((item) => {
+  if (!shell.which(item)) {
+    shell.echo(`Sorry, this script requires ${item}`)
+    shell.exit(1)
+  } else {
+    shell.echo(`The ${item} command can be run`)
+  }
+})
 
-//检查控制台是否以运行`git `开头的命令
-if (!shell.which('git')) {
-  //在控制台输出内容
-  shell.echo('Sorry, this script requires git')
-  shell.exit(1)
-}
-if (shell.exec('git add .').code !== 0) {
-  shell.echo('Error: Git add failed')
-  shell.exit(1)
-}
-if (shell.exec(`git commit -am "${name}"`).code !== 0) {
-  shell.echo('Error: Git commit failed')
-  shell.exit(1)
-}
-if (shell.exec('git push origin main').code !== 0) {
-  shell.echo('Error: Git push Branch main')
-  shell.exit(1)
-}
-if (
-  shell.exec('git subtree push --prefix docs/public origin docs').code !== 0
-) {
-  shell.echo('Error: Git push Branch main')
-  shell.exit(1)
-}
+const command = [
+  'pnpm docs:build',
+  'git add .',
+  `git commit -am ${argv.commit ? argv.commit : commitInfo}`,
+  'git push origin main',
+  'git subtree push --prefix docs/public origin docs'
+]
+// 执行脚本
+command.forEach((item) => {
+  if (shell.exec(item).code !== 0) {
+    shell.echo(`Error: ${item} failed`)
+    shell.exit(1)
+  }
+})
+
+// //检查控制台是否以运行`git `开头的命令
+// if (!shell.which('pnpm')) {
+//   //在控制台输出内容
+//   shell.echo('Sorry, this script requires pnpm')
+//   shell.exit(1)
+// }
+
+// if (shell.exec('pnpm docs:build').code !== 0) {
+//   shell.echo('Error: pnpm docs:build failed')
+//   shell.exit(1)
+// }
+
+// //检查控制台是否以运行`git `开头的命令
+// if (!shell.which('git')) {
+//   //在控制台输出内容
+//   shell.echo('Sorry, this script requires git')
+//   shell.exit(1)
+// }
+// if (shell.exec('git add .').code !== 0) {
+//   shell.echo('Error: Git add failed')
+//   shell.exit(1)
+// }
+// if (shell.exec(`git commit -am "${name}"`).code !== 0) {
+//   shell.echo('Error: Git commit failed')
+//   shell.exit(1)
+// }
+// if (shell.exec('git push origin main').code !== 0) {
+//   shell.echo('Error: Git push Branch main')
+//   shell.exit(1)
+// }
+// if (
+//   shell.exec('git subtree push --prefix docs/public origin docs').code !== 0
+// ) {
+//   shell.echo('Error: Git push Branch main')
+//   shell.exit(1)
+// }
