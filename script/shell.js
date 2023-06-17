c125f0795f06da0bdd0711f6554f24c912b0d301
lizhijie429 <632163606@qq.com>
2023-06-17 21:49:05 +0800

    Auto-commit

--- script/shell.js
+++ script/shell.js
@@ -2,7 +2,6 @@ import { createRequire } from 'module'
 const require = createRequire(import.meta.url)
 let shell = require('shelljs')
 let name = process.argv[2] || 'Auto-commit'
-let exec = shell.exec
 
 //检查控制台是否以运行`git `开头的命令
 if (!shell.which('git')) {
@@ -10,19 +9,21 @@ if (!shell.which('git')) {
   shell.echo('Sorry, this script requires git')
   shell.exit(1)
 }
-if (exec('git add .').code !== 0) {
-  echo('Error: Git add failed')
-  exit(1)
+if (shell.exec('git add .').code !== 0) {
+  shell.echo('Error: Git add failed')
+  shell.exit(1)
 }
-if (exec(`git commit -am "${name}"`).code !== 0) {
-  echo('Error: Git commit failed')
-  exit(1)
+if (shell.exec(`git commit -am "${name}"`).code !== 0) {
+  shell.echo('Error: Git commit failed')
+  shell.exit(1)
 }
-if (exec('git push origin main').code !== 0) {
-  echo('Error: Git push Branch main')
-  exit(1)
+if (shell.exec('git push origin main').code !== 0) {
+  shell.echo('Error: Git push Branch main')
+  shell.exit(1)
 }
-if (exec('git subtree push --prefix docs/public origin docs').code !== 0) {
-  echo('Error: Git push Branch docs')
-  exit(1)
+if (
+  shell.exec('git subtree push --prefix docs/public origin docs').code !== 0
+) {
+  shell.echo('Error: Git push Branch main')
+  shell.exit(1)
 }
